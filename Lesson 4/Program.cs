1136a556285ead86dd87c7128fd68722843d3edc
Stanislav <utkinkz@mail.ru>
2023-01-20 17:21:27 +0600

    Задача 4

---
+++ Lesson 4/Program.cs
@@ -0,0 +1,8 @@
+﻿// Написать программу которая на вход принимает число (n), ф на выходе показывает все четные числа от 1 до n.
+
+Console.WriteLine("Введите число ");
+int n = Convert.ToInt32(Console.ReadLine());
+for (int i = 2; i <= n; i+=2)
+{
+    Console.WriteLine($"{i}" );
+}
